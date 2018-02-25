03795f19ee14ff1cc7104ac8509336f52e17c190
pirana1996 <pirana_coder@hotmail.com>
2018-02-25 19:45:57 +0100

    new ver 4

--- src/Glavna.java
+++ src/Glavna.java
@@ -3,9 +3,5 @@ import java.util.Scanner;
 public class Glavna {
     public static void main(String[] args){
         int a = 1;
-        System.out.println("Koce");
-        System.out.println("HAHasA");
-        System.out.println("TRALALA");
-        System.out.println("JANA");
     }
 }
