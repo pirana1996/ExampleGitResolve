fde45d67b122b3f4e199e3fc861baafc8130055c
pirana1996 <pirana_coder@hotmail.com>
2018-02-25 20:27:13 +0100

    After pull

--- src/Glavna.java
+++ src/Glavna.java
@@ -7,22 +7,5 @@ public class Glavna {
         if(tmp1==1)
             System.out.println("Top 7");
         
-        System.out.println("xxx");
-        int c = 0;
-        System.out.println("FP");
-        System.out.println("Koce");
-        int a = 2;
-        int b = -1;
-        System.out.println("Kiril");
-        System.out.println("Daskalov");
-        int f = -10;
-        int g = 10;
-        int xxx = 69;
-        int y = 13;
-        int k = 14;
-        System.out.println("K");
-
-        System.out.println("Mleko");
-        System.out.println("Kakao");
     }
 }
