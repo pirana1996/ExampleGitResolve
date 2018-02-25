08cdcf406ab81822953475d925a0115596bca5dc
Kostadin <pirana_coder@hotmail.com>
2018-02-25 20:19:12 +0100

    Update Glavna.java

--- src/Glavna.java
+++ src/Glavna.java
@@ -3,6 +3,9 @@ public class Glavna {
         System.out.println("koce");
         System.out.println("Nade");
         System.out.println("Mirko");
+        int a = 1;
+        if(a==1)
+            System.out.println("Hey");
         System.out.println("Jana");
         System.out.println("Trajko");
     }
