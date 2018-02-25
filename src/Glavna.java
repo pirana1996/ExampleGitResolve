306a8aaff916c36ca05e09ce1f6547ad31218c32
pirana1996 <pirana_coder@hotmail.com>
2018-02-25 18:43:11 +0100

    trajko is added

--- src/Glavna.java
+++ src/Glavna.java
@@ -2,5 +2,6 @@ public class Glavna {
     public static void main(String[] args){
         System.out.println("koce");
         System.out.println("Nade");
+        System.out.println("Trajko");
     }
 }
