f96fa8f0582929b2c186f56eed3488dc5019df47
pirana1996 <pirana_coder@hotmail.com>
2018-02-25 19:32:35 +0100

    new ver 1

--- src/Glavna.java
+++ src/Glavna.java
@@ -2,16 +2,7 @@ import java.util.Scanner;
 
 public class Glavna {
     public static void main(String[] args){
-        System.out.println("koce");
-        System.out.println("Nade");
-        System.out.println("Mirko");
-        System.out.println("Chorbev");
-        int a = 1;
-        if(a==1)
-            System.out.println("Hey");
-        System.out.println("Jana");
-        System.out.println("Trajko");
-        Scanner input = new Scanner(System.in);
+
         System.out.println("HAHA");
     }
 }
