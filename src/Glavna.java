a6330a68439e6797e80f20edf5d9c43f296d0395
Kostadin <pirana_coder@hotmail.com>
2018-02-25 20:45:39 +0100

    Update Glavna.java

--- src/Glavna.java
+++ src/Glavna.java
@@ -2,10 +2,6 @@ import java.util.Scanner;
 
 public class Glavna {
     public static void main(String[] args){
-        int a = 1;
-        System.out.println("Koce");
-        System.out.println("HAHasA");
-        System.out.println("TRALALA");
         System.out.println("JANA");
     }
 }
