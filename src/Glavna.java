c18358ef4884ec30cfaa9fd09e3eea58735ae3b0
Kostadin <pirana_coder@hotmail.com>
2018-02-25 20:13:27 +0100

    Update Glavna.java

--- src/Glavna.java
+++ src/Glavna.java
@@ -2,6 +2,7 @@ public class Glavna {
     public static void main(String[] args){
         System.out.println("koce");
         System.out.println("Nade");
+        System.out.println("Jana");
         System.out.println("Trajko");
     }
 }
